180b677871d685bbfb70b469987a9216559670e9
Hyxper <jack.sargeant22@gmail.com>
2022-08-01 16:37:12 +0100

    Finished section 2

--- app.ts
+++ app.ts
@@ -1,40 +1,16 @@
+let userInput: unknown;
+let userName: string;
 
-enum dt {
-     STRING = "STRING",
-     NUMERIC = "NUMERIC"
+userInput = 5;
+userInput = "123";
+
+
+if (typeof userInput === "string") {
+    userName = userInput;
 }
 
-type Combinable = string | number;
-type EnumCombinable = dt.NUMERIC | dt.STRING;
-
-
-
-function combine(
-     ip1: Combinable ,
-     ip2: Combinable, 
-     resultType: EnumCombinable ){
-    
-    let result;
-
-    if (typeof ip1 === "number" && typeof ip2 === "number") {
-         result = ip1 + ip2;
-    }else{
-         result = ip1.toString()+ " " + ip2.toString()
-    }
-
-    if(typeof result === "string" && resultType === dt.NUMERIC){
-          throw new Error ("INCORRECT COMBINATION!!")
-    }else if (resultType === dt.NUMERIC) {
-          return +result;
-    }else{
-          return result.toString();
-    }
-
+function generateErr(msg: string, code: number): never{
+    throw {msg: msg, code: code};
 }
 
-const combinedAges = combine(30,26,dt.NUMERIC);
-console.log(combinedAges);
-const combinedName = combine("Jack","Sargeant",dt.STRING);
-console.log(combinedName);
-const combiedStringAges = combine("21","25",dt.STRING);
-const flushErr = combine("JACK","SARGE",dt.NUMERIC);
+generateErr("ERROR",500)
